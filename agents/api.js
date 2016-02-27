395bce3f625b2df370e5b03679b0f0458791f3f1
Andrew Forth <7andpiper@gmail.com>
2016-02-27 18:01:05 -0500

    Wrap api server loading in function and ensure that requests collection index exists on startup

--- agents/api.js
+++ agents/api.js
@@ -26,8 +26,16 @@ var hostConfig = require('../config/host.json')
 var dbConn;
 databaseSrvc.getConnection()
     .then(function (conn) {
-        dbConn = conn;
+
         chunkerSrvc.setDbConn(conn);
+        dbConn = conn;
+
+        dbConn.collection('requests').createIndex(
+            { "requestId": 1 },
+            { "background": 1 },
+        function (err, results) {
+            loadServer();
+        });
     });
 
 var handleProcessExit = function () {
@@ -40,70 +48,77 @@ process.on('SIGHUP', handleProcessExit);
 process.on('SIGTERM', handleProcessExit);
 process.on('uncaughtException', handleProcessExit);
 
-/**
- * Creates and configures the server
- */
-var server = Restify.createServer({
-    name: hostConfig.name + ' API',
-    version: '1.0.0'
-}).use(Restify.bodyParser());
 
 /**
- * Provides an easy way to ascertain if the api is up
+ * Encapsulate loading of the server
  */
-server.get('/ping', function (req, res, next) {
-    res.send(httpSrvc.OK, {
+var loadServer = function () {
+
+    /**
+     * Creates and configures the server
+     */
+    var server = Restify.createServer({
         name: hostConfig.name + ' API',
-        status: 'available'
+        version: '1.0.0'
+    }).use(Restify.bodyParser());
+
+    /**
+     * Provides an easy way to ascertain if the api is up
+     */
+    server.get('/ping', function (req, res, next) {
+        res.send(httpSrvc.OK, {
+            name: hostConfig.name + ' API',
+            status: 'available'
+        });
+        next();
     });
-    next();
-});
 
-/**
- * Queues text body for counting
- */
-server.post('/queue', function (req, res) {
+    /**
+     * Queues text body for counting
+     */
+    server.post('/queue', function (req, res) {
 
-    chunkerSrvc.queue(req.params)
-        .then(function (result) {
-            res.status(httpSrvc.OK);
-            res.json(result);
-        })
-        .catch(function (err) {
-            res.status(err.code || httpSrvc.INTERNAL_ERROR); // Error codes are reflected in the response body in order to control exactly what error info is available to the client.
-            res.json({
-                message: err.message || 'Request refused'
+        chunkerSrvc.queue(req.params)
+            .then(function (result) {
+                res.status(httpSrvc.OK);
+                res.json(result);
+            })
+            .catch(function (err) {
+                res.status(err.code || httpSrvc.INTERNAL_ERROR); // Error codes are reflected in the response body in order to control exactly what error info is available to the client.
+                res.json({
+                    message: err.message || 'Request refused'
+                });
             });
-        });
-});
+    });
 
-/**
- * Gets results based on a uuid
- */
-server.get('/results/:requestId', function (req, res, next) {
+    /**
+     * Gets results based on a uuid
+     */
+    server.get('/results/:requestId', function (req, res, next) {
 
-    resultsSrvc.getResults(req.params)
-        .then(function (result) {
-            res.send(httpSrvc.OK, result);
-        })
-        .catch(function (err) {
-            res.send(err.code, err.message);
-        })
-        .finally(function () {
-            next();
-        });
-});
+        resultsSrvc.getResults(req.params)
+            .then(function (result) {
+                res.send(httpSrvc.OK, result);
+            })
+            .catch(function (err) {
+                res.send(err.code, err.message);
+            })
+            .finally(function () {
+                next();
+            });
+    });
 
-/**
- * Starts the server
- */
-server.listen(process.env.PORT || hostConfig.port, hostConfig.host, function (err) {
-    if (err) {
-        var msg = 'API server failed on startup';
-        throw new Error(msg);
-    } else {
-        console.log('-------------------------------------------------------------------------------');
-        console.log(' Starting ' + server.name + ' - ', server.address());
-        console.log('-------------------------------------------------------------------------------');
-    }
-});
+    /**
+     * Starts the server
+     */
+    server.listen(process.env.PORT || hostConfig.port, hostConfig.host, function (err) {
+        if (err) {
+            var msg = 'API server failed on startup';
+            throw new Error(msg);
+        } else {
+            console.log('-------------------------------------------------------------------------------');
+            console.log(' Starting ' + server.name + ' - ', server.address());
+            console.log('-------------------------------------------------------------------------------');
+        }
+    });
+};

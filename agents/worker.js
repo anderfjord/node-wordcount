ad813326ff381052d099fae9ba5f688360d154cb
Andrew Forth <7andpiper@gmail.com>
2016-02-27 10:20:57 -0500

    Offload most worker functionality to services

--- agents/worker.js
+++ agents/worker.js
@@ -7,114 +7,12 @@
  */
 
 /**
- * Package Dependencies
- */
-var Promise = require('bluebird');
-
-/**
  * Local Dependencies
  */
 var hostConfig = require('../config/host.json')
-  , httpSrvc = require('../lib/services/http')
-  , amqpSrvc = require('../lib/services/amqp');
-
-
-/**
- * @param string chunk
- * @return Promise
- * @resolve object
- */
-var processChunk = function (chunk) {
-
-    return new Promise(function (resolve, reject) {
-
-        try {
-            chunk = JSON.parse(chunk);
-        } catch (err) {
-            reject(err);
-        }
-
-        determineTopWords(chunk)
-            .then(function (topWords) {
-
-                saveResults(chunk.requestId, topWords)
-                    .then(function () {
-                        resolve();
-                    });
-            });
-    });
-};
-
-/**
- * @param string chunk
- * @return Promise
- * @resolve object
- */
-var determineTopWords = function (chunk) {
-
-    return new Promise(function (resolve, reject) {
-
-        var topThreshold
-          , topWords = []
-          , wordCounts = {}
-          , words = chunk.text.split(' ')
-          , word;
-
-        // Keep just the top 10 if the input all fit in a single chunk
-        if (parseInt(chunk.totalChunks, 10) === 1) {
-            topThreshold = 10;
-        }
-        // Keep more if there are multiple chunks, in order to avoid disparities in counts across chunks
-        else {
-            topThreshold = 50;
-        }
-
-        words.forEach(function (word) {
-            if (word === '' || word === ' ') {
-                return;
-            }
-            else if (typeof wordCounts[word] === 'undefined') {
-                wordCounts[word] = 1;
-            }
-            else {
-                wordCounts[word] += 1;
-            }
-        });
-
-        var counts = [];
-
-        for (word in wordCounts) {
-            counts.push({
-                word: word,
-                count: wordCounts[word]
-            });
-        }
-
-        topWords = counts.sort(function (a, b) {
-            return (a.count > b.count) ? -1 : ((a.count < b.count) ? 1 : 0);
-        });
-
-        topWords = topWords.slice(0, topThreshold);
-
-        console.log('TOP WORDS: ', topWords);
-
-        resolve(topWords);
-    });
-};
-
-/**
- * @param string requestId
- * @param object topWords
- * @return Promise
- * @resolve
- */
-var saveResults = function (requestId, topWords) {
-
-    return new Promise(function (resolve, reject) {
-
-        // @TODO - store results in a database
-    });
-};
+  , amqpSrvc = require('../lib/services/amqp')
+  , counterSrvc = require('../lib/services/counter')
+  , resultsSrvc = require('../lib/services/results');
 
 /**
  * Immediatly acquire an AMQP channel and start consuming
@@ -128,9 +26,22 @@ amqpSrvc.getChannel(hostConfig.amqp)
                 
                 channel.ack(msg);
 
-                processChunk(msg.content.toString())
-                    .catch(function (err) {
-                        console.error('Error occurred processing chunk: ', err);
+                var chunk;
+
+                try {
+                    chunk = JSON.parse(msg.content.toString());
+                } catch (err) {
+                    console.error('Malformed chunk: ', err);
+                    return;
+                }
+
+                // Extract the top words from the chunk
+                var topWords = counterSrvc.getTopWords(chunk)
+                    
+                // Store the results of the chunk
+                resultsSrvc.storeResults(chunk.requestId, topWords, chunk.totalChunks)
+                    .then(function () {
+                        console.log('Done processing and storing chunk: ', chunk.requestId, ' -> 1 of ' + chunk.totalChunks);
                     });
             }
         });

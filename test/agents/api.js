be865a812cdfc125297421c8caea3873750d6170
Andrew Forth <7andpiper@gmail.com>
2016-02-27 18:17:42 -0500

    Fix api unit tests for error conditions

--- test/agents/api.js
+++ test/agents/api.js
@@ -12,6 +12,7 @@ var should = require('should')
  * Local Dependencies
  */
 var hostConfig = require('../../config/host.json')
+  , httpSrvc = require('../../lib/services/http')
   , uuidSrvc = require('../../lib/services/uuid');
 
 
@@ -19,7 +20,8 @@ describe('Wordcount API', function() {
 
     var Client
       , sampleTextFile = 'data/sample.txt'
-      , sampleText = '';
+      , sampleText = ''
+      , currentRequestId;
 
     before(function(done) {
 
@@ -71,6 +73,22 @@ describe('Wordcount API', function() {
                 obj.textSize.should.equal(sampleText.length);
                 obj.totalChunks.should.be.Number;
                 obj.totalChunks.should.be.greaterThan(-1);
+
+                currentRequestId = obj.requestId;
+
+                done();
+            });
+        });
+
+        it('should return an error object when text property is missing', function (done) {
+
+            Client.post('/queue', {}, function (err, req, res, obj) {
+            
+                if (!err) { return done('Text property was not present in request but no error thrown'); }
+
+                obj.should.be.an.Object;
+                obj.should.have.properties(['code', 'message']);
+                httpSrvc.isErrorStatus(obj.code).should.be.true;
                 done();
             });
         });
@@ -80,7 +98,7 @@ describe('Wordcount API', function() {
 
         it('should return an object containing certain fields in case of success', function (done) {
 
-            Client.get('/results/00193c01-4bbc-4191-a142-360e090332fe', function (err, req, res, obj) {
+            Client.get('/results/' + currentRequestId, function (err, req, res, obj) {
             
                 if (err) { return done(err); }
 
@@ -99,19 +117,13 @@ describe('Wordcount API', function() {
 
         it('should return an object containing certain fields in case of failure', function (done) {
 
-            Client.get('/results/00193c01-4bbc-4191-a142-360e090332fe', function (err, req, res, obj) {
+            Client.get('/results/12345678-cccc-bbbb-aaaa-1234abcd9876', function (err, req, res, obj) {
             
-                if (err) { return done(err); }
+                if (!err) { return done('Request id does not exist but no error thrown'); }
 
                 obj.should.be.an.Object;
-                obj.should.have.properties(['requestId', 'textSize', 'status', 'completedChunks', 'totalChunks', 'finalCounts']);
-                uuidSrvc.validate(obj.requestId).should.be.true;
-                obj.textSize.should.be.Number;
-                ['in_progress', 'completed'].indexOf(obj.status).should.be.greaterThan(-1);
-                obj.completedChunks.should.be.Number;
-                obj.totalChunks.should.be.Number;
-                obj.finalCounts.should.be.Object;
-                
+                obj.should.have.properties(['code', 'message']);
+                httpSrvc.isErrorStatus(obj.code).should.be.true;
                 done();
             });
         });

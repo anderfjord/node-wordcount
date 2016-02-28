15cbe4e0f01b56cb8b3d9dc731b619f89459a744
Andrew Forth <7andpiper@gmail.com>
2016-02-27 19:17:26 -0500

    Expand test coverage of chunker service

--- test/lib/services/chunker.js
+++ test/lib/services/chunker.js
@@ -30,6 +30,37 @@ describe('Chunker Service', function() {
             });
     });
 
+    describe('loadAmqpChannel', function() {
+
+        it('should resolve a channel object when no config is explicitly supplied', function () {
+            var amqpChannel = chunkerSrvc.loadAmqpChannel();
+            amqpChannel.should.be.an.Object;
+            amqpChannel.should.not.be.false;
+        });
+
+        it('should resolve a channel object when a custom config is explicitly supplied', function () {
+            var amqpChannel = chunkerSrvc.loadAmqpChannel({
+                user: 'guest',
+                pass: 'guest',
+                host: 'localhost',
+                queue: 'wordcount'
+            });
+            amqpChannel.should.be.an.Object;
+            amqpChannel.should.not.be.false;
+        });
+    });
+
+    describe('setDbConn', function() {
+
+        it('should not throw an error when an object is supplied', function () {
+            chunkerSrvc.setDbConn.bind(null, {}).should.not.throw();
+        });
+
+        it('should throw an error when a non-object is supplied', function () {
+            chunkerSrvc.setDbConn.bind(null, null).should.throw();
+        });
+    });
+
     describe('getChunks', function() {
 
         it('should return an array with a single value when max chunk size is not supplied', function () {

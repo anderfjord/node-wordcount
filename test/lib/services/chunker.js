c2c3a727768d27a3302fd78f97ce2a9c9dc47e10
Andrew Forth <7andpiper@gmail.com>
2016-02-27 20:16:17 -0500

    Expand tests for chunker service

--- test/lib/services/chunker.js
+++ test/lib/services/chunker.js
@@ -10,7 +10,9 @@ var should = require('should')
 /**
  * Local Dependencies
  */
-var chunkerSrvc = require('../../../lib/services/chunker');
+var chunkerSrvc = require('../../../lib/services/chunker')
+  , uuidSrvc = require('../../../lib/services/uuid')
+  , databaseSrvc = require('../../../lib/services/database').load('mongo');
 
 
 describe('Chunker Service', function() {
@@ -70,11 +72,76 @@ describe('Chunker Service', function() {
             chunks.length.should.equal(1);
         });
 
-        // it('should return an array with multiple values when a small max chunk size is supplied', function () {
-        //     var chunks = chunkerSrvc.getChunks(sampleText, 10);
-        //     chunks.should.be.an.Array;
-        //     chunks.length.should.be.Number;
-        //     chunks.length.should.be.greaterThan(1);
-        // });
+        it('should return an array with a single value when max chunk size is supplied, but sufficiently low', function () {
+            var chunks = chunkerSrvc.getChunks(sampleText, 50000);
+            chunks.should.be.an.Array;
+            chunks.length.should.be.Number;
+            chunks.length.should.equal(1);
+        });
+
+        it('should return an array with a single value when max chunk size is supplied, but exceeding max config size', function () {
+            var chunks = chunkerSrvc.getChunks(sampleText, 100000);
+            chunks.should.be.an.Array;
+            chunks.length.should.be.Number;
+            chunks.length.should.equal(1);
+        });
+
+    });
+
+    describe('queue', function() {
+
+        var dbConn;
+
+        before(function(done) {
+        
+            databaseSrvc.getConnection()
+                .then(function (conn) {
+                    chunkerSrvc.setDbConn(conn);
+                    dbConn = conn;
+                    done();
+                });
+        });
+
+        after(function() {
+            databaseSrvc.closeConnection(dbConn);
+        });
+
+        it('should throw an error when the text property is not defined', function (done) {
+            chunkerSrvc.queue({})
+                .then(function () {
+                    done('chunkerSrvc.queue should throw when no text property is defined');
+                })
+                .catch(function (err) {
+                    done();
+                });
+        });
+
+        it('should throw an error when the text property is not a string', function (done) {
+            chunkerSrvc.queue({text: 512})
+                .then(function () {
+                    done('chunkerSrvc.queue should throw when no text property is defined');
+                })
+                .catch(function (err) {
+                    done();
+                });
+        });
+
+        it('should resolve an object when the text property is defined and is a string', function (done) {
+            chunkerSrvc.queue({text: sampleText})
+                .then(function (result) {
+                    result.should.be.an.Object;
+                    result.should.have.properties(['requestId', 'textSize', 'totalChunks']);
+                    uuidSrvc.validate(result.requestId).should.be.true;
+                    result.textSize.should.be.a.Number;
+                    result.textSize.should.be.greaterThan(0);
+                    result.totalChunks.should.be.a.Number;
+                    result.totalChunks.should.be.greaterThan(0);
+                    done();
+                })
+                .catch(function (err) {
+                    done(err);
+                });
+        });
+
     });
 });

8731fa853063fe2279a6190c3865105c6f068135
Andrew Forth <7andpiper@gmail.com>
2016-02-27 20:32:47 -0500

    Tweaks to mongo driver and expanded test coverage

--- test/lib/drivers/mongo.js
+++ test/lib/drivers/mongo.js
@@ -33,14 +33,34 @@ describe('MongoDB Driver', function() {
                     done();
                 });
         });
+
+        it('should throw an error if an empty config is supplied', function (done) {
+            mongoDriver.getConnection({})
+                .then(function () {
+                    done('Error should have been thrown');
+                })
+                .catch(function (err) {
+                    done();
+                });
+        });
     });
 
     describe('closeConnection', function() {
+
         it('should close a connection', function (done) {
             mongoDriver.getConnection()
                 .then(function (conn) {
                     conn.should.be.an.Object;
                     mongoDriver.closeConnection(conn);
+                    done();
+                });
+        });
+
+        it('should do nothing if no connection is passed', function (done) {
+            mongoDriver.getConnection()
+                .then(function (conn) {
+                    conn.should.be.an.Object;
+                    mongoDriver.closeConnection();
                     done();
                 });
         });
